6398246497080fa6680b91a6bf5850a8ed5c7246
Mark Bell <me@markashleybell.com>
2013-11-03 10:53:57 +0000

    Reorganise code for PEP8

--- ExpandTabsOnLoad.py
+++ ExpandTabsOnLoad.py
@@ -1,10 +1,16 @@
-import sublime, sublime_plugin, os, re
+import os
+import re
+import sublime
+import sublime_plugin
+
 
 class ExpandTabsOnLoad(sublime_plugin.EventListener):
     # Run ST's 'expand_tabs' command when opening a file,
     # only if there are any tab characters in the file
     def on_load(self, view):
-        if view.settings().get("expand_tabs_on_load", False) and view.find("\t", 0):
+        expand_tabs = view.settings().get("expand_tabs_on_load", False)
+        if expand_tabs and view.find("\t", 0):
             view.run_command("expand_tabs")
-            message = "Converted tab characters to {0} spaces".format(view.settings().get("tab_size", 0))
+            tab_size = view.settings().get("tab_size", 0)
+            message = "Converted tab characters to {0} spaces".format(tab_size)
             sublime.status_message(message)
