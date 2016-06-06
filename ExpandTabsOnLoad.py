05377ab86647bce9363d4fbba52f78f33298388b
Mark Bell <me@markb.co.uk>
2016-06-06 10:47:46 +0100

    Fixed for Sublime Text 3, closes #1

--- ExpandTabsOnLoad.py
+++ ExpandTabsOnLoad.py
@@ -10,7 +10,7 @@ class ExpandTabsOnLoad(sublime_plugin.EventListener):
     def on_load(self, view):
         expand_tabs = view.settings().get("expand_tabs_on_load", False)
         if expand_tabs and view.find("\t", 0):
-            view.run_command("expand_tabs")
+            view.run_command("expand_tabs", {"set_translate_tabs": True})
             tab_size = view.settings().get("tab_size", 0)
             message = "Converted tab characters to {0} spaces".format(tab_size)
             sublime.status_message(message)
